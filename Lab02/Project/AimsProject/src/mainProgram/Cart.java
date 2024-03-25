c0353db0c67dcf896dac54c52de254096dab0120
Thanh-Cong-BH <thanhcongbh1502@gmail.com>
2024-03-25 11:08:34 +0700

    Lab02 classwork submission

---
+++ Lab02/Project/AimsProject/src/mainProgram/Cart.java
@@ -0,0 +1,9 @@
+package mainProgram;
+
+public class Cart {
+	public static final int MAX_NUMBER_ORDERED = 20;
+	private DigitalVideoDisc itemsOrdered[] = 
+			new DigitalVideoDisc[MAX_NUMBER_ORDERED];
+	
+
+}
